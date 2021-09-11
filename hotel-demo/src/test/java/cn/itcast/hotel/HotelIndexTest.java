606d62c1c7ca4a83fbaa5c698cf0b6be3ce6a764
Lanqilu <885240677@qq.com>
2021-09-11 14:58:45 +0800

    创建索引库

--- hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
+++ hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -1,13 +1,18 @@
 package cn.itcast.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static cn.itcast.hotel.HotelConstants.MAPPING_TEMPLATE;
 
 /**
  * @author Halo
@@ -23,6 +28,18 @@ public class HotelIndexTest {
     void testInit() {
         System.out.println("client = " + client);
     }
+
+    @Test
+    void createHotelIndex() throws IOException {
+        // 1.创建Request对象
+        CreateIndexRequest request = new CreateIndexRequest("hotel");
+        // 2.准备请求的参数：DSL语句
+        request.source(MAPPING_TEMPLATE, XContentType.JSON);
+        // 3.发送请求
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+
 
     @BeforeEach
     void setUp() {

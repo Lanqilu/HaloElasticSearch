97780b283b2d2c01ef9c23de22ca2877d0dc6137
Lanqilu <885240677@qq.com>
2021-09-12 19:17:26 +0800

    酒店搜索和分页

---
+++ hotel-demo/src/main/java/cn/itcast/hotel/controller/HotelController.java
@@ -0,0 +1,28 @@
+package cn.itcast.hotel.controller;
+
+import cn.itcast.hotel.pojo.PageResult;
+import cn.itcast.hotel.pojo.RequestParams;
+import cn.itcast.hotel.service.IHotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Halo
+ * @create 2021/09/12 下午 06:43
+ * @description
+ */
+@RestController
+@RequestMapping("/hotel")
+public class HotelController {
+
+    @Autowired
+    private IHotelService hotelService;
+    // 搜索酒店数据
+    @PostMapping("/list")
+    public PageResult search(@RequestBody RequestParams params){
+        return hotelService.search(params);
+    }
+}

e12541710eb5dc2bcf9e42378bcedd25b2b200ac
Lanqilu <885240677@qq.com>
2021-09-12 11:06:43 +0800

    RestAPI

--- hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
+++ hotel-demo/src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -1,10 +1,12 @@
 package cn.itcast.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,7 +41,23 @@ public class HotelIndexTest {
         client.indices().create(request, RequestOptions.DEFAULT);
     }
 
+    @Test
+    void testDeleteHotelIndex() throws IOException {
+        // 1.创建Request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("hotel");
+        // 2.发送请求
+        client.indices().delete(request, RequestOptions.DEFAULT);
+    }
 
+    @Test
+    void testExistsHotelIndex() throws IOException {
+        // 1.创建Request对象
+        GetIndexRequest request = new GetIndexRequest("hotel");
+        // 2.发送请求
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        // 3.输出
+        System.err.println(exists ? "索引库已经存在！" : "索引库不存在！");
+    }
 
     @BeforeEach
     void setUp() {
